86a2d9edee9c0dbab66797a9fdf80d994ef30329
浆糊 <14785860234@163.com>
2025-03-11 18:51:02 +0800

    Site updated: 2025-03-11 18:51:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/11/JUC/","2025/03/11/算法/"];function toRandomPost(){
+var posts=["2025/03/11/算法/","2025/03/11/JUC/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
